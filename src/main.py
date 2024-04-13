ab91f385694e2b7a74b5a2366f15a300e6c0971c
QuanD <42784039+KevinDo76@users.noreply.github.com>
2024-04-13 10:41:32 -0700

    fixing pycharm thing

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 from mapObj import mapObj
 import pygame as pg
+
+
 def main():
     running = True
     pg.init()
     mainDisplay = pg.display.set_mode((800, 600))
     pg.display.set_caption('Yokai')
-    #main loop
+    # main loop
     while running:
         for event in pg.event.get():
             if event.type == pg.QUIT:
@@ -13,6 +15,7 @@ def main():
 
         mainDisplay.fill((255, 255, 255))
         pg.display.flip()
+
 
 if __name__ == '__main__':
     main()
