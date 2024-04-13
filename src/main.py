8c5dc2fa4e9f68a8229f9d81b7cf93a4c0dada5f
QuanD <42784039+KevinDo76@users.noreply.github.com>
2024-04-13 10:39:09 -0700

    setting up pygame/screen

--- src/main.py
+++ src/main.py
@@ -1,10 +1,18 @@
 from mapObj import mapObj
 import pygame as pg
 def main():
+    running = True
     pg.init()
-    mainDisplay =
-    mainMap = mapObj()
-    print(mainMap.lilTest)
+    mainDisplay = pg.display.set_mode((800, 600))
+    pg.display.set_caption('Yokai')
+    #main loop
+    while running:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+
+        mainDisplay.fill((255, 255, 255))
+        pg.display.flip()
 
 if __name__ == '__main__':
     main()
