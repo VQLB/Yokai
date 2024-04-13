50793a10f1a620f051de74f272a30eb48e544d89
QuanD <42784039+KevinDo76@users.noreply.github.com>
2024-04-13 11:48:07 -0700

    got camera work

--- src/cameraObj.py
+++ src/cameraObj.py
@@ -1,8 +1,13 @@
+import pygame
 class Camera:
-    def __init__(self, pos: tuple[int, int]):
-        self.posX = pos[0]
-        self.posY = pos[1]
+    def __init__(self, pos: tuple[float, float]):
+        self.position = pos
         self.zoom = 0
 
-    def convert(self, pos: tuple[int ,int]):
-        pass
+    def convertWorldToScreen(self, pos: tuple[float ,float], surface: pygame.surface) -> tuple[float, float]:
+        returnPos = list(pos)
+        screenSize = surface.get_size()
+        returnPos[0] += self.position[0] + screenSize[0]/2
+        returnPos[1] += self.position[1] + screenSize[1]/2
+        print(returnPos)
+        return tuple(returnPos)
--- src/mapObj.py
+++ src/mapObj.py
@@ -5,11 +5,10 @@ class Map():
 
 
     def __init__(self, pos: tuple[float, float], texturePath: str):
-        self.posX = pos[0]
-        self.posY = pos[1]
+        self.position = pos
         self.surfaceTexture = pygame.image.load(texturePath).convert()
 
     def render_self(self, screen: pg.Surface, camera: Camera):
-        textureSize = self.surfaceTexture.get_size()
-        screen.blit(pygame.transform.scale(self.surfaceTexture, (100,100)), (self.posX + camera.posY, self.posY + camera.posX))
+        screenPos = camera.convertWorldToScreen(self.position, screen)
+        screen.blit(self.surfaceTexture, screenPos)
 
