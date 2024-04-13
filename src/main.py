3e8bdb98d0919ce489c13dc8ba0595451a342410
Ben Buzard <ben@benbuzard.com>
2024-04-13 14:00:32 -0700

    Add initial inventory

--- src/main.py
+++ src/main.py
@@ -1,17 +1,22 @@
 from mapObj import Map
 import pygame as pg
 from cameraObj import Camera
+from src.ui.UIManager import UIManager
 from textureatlas import TextureAtlas
 from hungerbar import HungerBar
+from ui.Inventory import Inventory
 
 FPS = 60
+WINDOW_SIZE = (800, 600)
 
 def main():
     # Initialization
     running = True
     pg.init()
-    MainSurface = pg.display.set_mode((800, 600))
+    MainSurface = pg.display.set_mode(WINDOW_SIZE)
     pg.display.set_caption('Yokai')
+
+    texture_atlas = TextureAtlas("asset/atlas.png")
     # Bars
     bar = HungerBar(20, 10, 300, 20, 100)
     # Main obj init
@@ -19,7 +24,12 @@ def main():
     MainCamera = Camera((500, 500))
     clock = pg.time.Clock()
 
-    texture_atlas = TextureAtlas("asset/atlas.png")
+    ui_manager = UIManager(WINDOW_SIZE)
+
+    inventory = Inventory(3, 5, inventory_tile=texture_atlas.get_sprite((4, 0)))
+
+    ui_manager.add_panel(inventory)
+    ui_manager.make_active(inventory)
 
     # Main loop
     while running:
@@ -34,6 +44,9 @@ def main():
 
         MainMap.render_self(MainSurface, MainCamera)
         bar.render_self(MainSurface)
+
+        ui_manager.render_self(MainSurface)
+
         pg.display.flip()
 
 
