9543b94809d96cf63328b7b5753b53282916b199
Lilian Miller <lilianmill20@gmail.com>
2024-04-13 16:02:36 -0700

    made start screen, press mouse down

--- src/main.py
+++ src/main.py
@@ -8,6 +8,8 @@ from src.ui.UIManager import UIManager
 from textureatlas import TextureAtlas
 from ui.Inventory import Inventory
 from entity.Character import Character
+from startscreen import StartScreen
+from button import Button
 
 FPS = 60
 WINDOW_SIZE = (800, 600)
@@ -18,6 +20,11 @@ def main():
     pg.init()
     MainSurface = pg.display.set_mode(WINDOW_SIZE)
     pg.display.set_caption('Yokai')
+
+    # start screen button
+    startscreen = StartScreen()
+
+
 
     texture_atlas = TextureAtlas("asset/atlas.png")
 
@@ -32,66 +39,77 @@ def main():
     MainCharacter = Character("asset/atlas.png")
     clock = pg.time.Clock()
 
+    # inventory
     ui_manager = UIManager(WINDOW_SIZE)
-
     inventory = Inventory(3, 5, inventory_tile=texture_atlas.get_sprite((4, 0)))
-
     ui_manager.add_panel(inventory)
+
+
 
     # Main loop
     while running:
         delta_time = clock.tick(FPS) / 1000.0
         print(delta_time)
 
-        keys = pygame.key.get_pressed()
-        mainCharVec = [0,0]
-        if keys[pg.K_w]:
-            mainCharVec[1]+=-1
-        if keys[pg.K_s]:
-            mainCharVec[1] += 1
-        if keys[pg.K_a]:
-            mainCharVec[0] +=-1
-        if keys[pg.K_d]:
-            mainCharVec[0] +=1
+        # if start screen visible, skip everything--else play everything else
+        if startscreen.active:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    running = False
+                elif event.type == pg.MOUSEBUTTONDOWN:
+                    startscreen.active = False
+            startscreen.render_self(MainSurface)
 
-        MainCharacter.moveDir(tuple(mainCharVec))
-
-
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                running = False
-            if event.type == pygame.MOUSEWHEEL:
-                MainCamera.zoom+=event.y*0.01
-            if event.type == pg.KEYDOWN:
-                if event.key == pg.K_e:
-                    ui_manager.toggle_active(inventory)
-
-        # Deplete health when hunger or thirst status bars are at 0
-        # TODO: make it so attacking deplete hunger
-        # TODO: make it so walking deplete thirst
-        if hungerbar.value <= 0:
-            hungerbar.value = 0
         else:
-            hungerbar.value -= .02
+            keys = pygame.key.get_pressed()
+            mainCharVec = [0,0]
+            if keys[pg.K_w]:
+                mainCharVec[1]+=-1
+            if keys[pg.K_s]:
+                mainCharVec[1] += 1
+            if keys[pg.K_a]:
+                mainCharVec[0] +=-1
+            if keys[pg.K_d]:
+                mainCharVec[0] +=1
 
-        if thirstbar.value <= 0:
-            thirstbar.value = 0
-        else:
-            thirstbar.value -= .03
+            MainCharacter.moveDir(tuple(mainCharVec))
 
-        if hungerbar.value == 0 or thirstbar.value == 0:
-            healthbar.value -= .5
 
-        MainSurface.fill((0, 0, 0))
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    running = False
+                if event.type == pygame.MOUSEWHEEL:
+                    MainCamera.zoom+=event.y*0.01
+                if event.type == pg.KEYDOWN:
+                    if event.key == pg.K_e:
+                        ui_manager.toggle_active(inventory)
 
-        MainCamera.position = (MainCharacter.position[0] + 50, MainCharacter.position[1] + 50)
+            # Deplete health when hunger or thirst status bars are at 0
+            # TODO: make it so attacking deplete hunger
+            # TODO: make it so walking deplete thirst
+            if hungerbar.value <= 0:
+                hungerbar.value = 0
+            else:
+                hungerbar.value -= .02
 
-        MainMap.render_self(MainSurface, MainCamera)
-        MainCharacter.render_self(MainSurface, MainCamera)
-        healthbar.render_self(MainSurface)
-        hungerbar.render_self(MainSurface)
-        thirstbar.render_self(MainSurface)
-        ui_manager.render_self(MainSurface)
+            if thirstbar.value <= 0:
+                thirstbar.value = 0
+            else:
+                thirstbar.value -= .03
+
+            if hungerbar.value == 0 or thirstbar.value == 0:
+                healthbar.value -= .5
+
+            MainSurface.fill((0, 0, 0))
+
+            MainCamera.position = (MainCharacter.position[0] + 50, MainCharacter.position[1] + 50)
+
+            MainMap.render_self(MainSurface, MainCamera)
+            MainCharacter.render_self(MainSurface, MainCamera)
+            healthbar.render_self(MainSurface)
+            hungerbar.render_self(MainSurface)
+            thirstbar.render_self(MainSurface)
+            ui_manager.render_self(MainSurface)
         pg.display.flip()
 
 
--- src/startscreen.py
+++ src/startscreen.py
@@ -1,1 +1,12 @@
 import pygame
+
+class StartScreen():
+    def __init__(self):
+        self.active = True
+
+
+    def render_self(self, surface: pygame.Surface):
+        pygame.draw.rect(surface, (34, 48, 38), (0, 0, 800, 800))
+
+
+
