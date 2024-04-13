09aa175959a498482e8404d1857ddff7f5c3608e
Ben Buzard <ben@benbuzard.com>
2024-04-13 11:34:10 -0700

    Modify TextureAtlas to use (x, y) tuple

--- src/textureatlas.py
+++ src/textureatlas.py
@@ -5,5 +5,6 @@ class TextureAtlas:
         self.atlas_path = atlas_path
         self.image = pygame.image.load(atlas_path)
 
-    def get_sprite(self, x: int, y: int) -> pygame.Surface:
+    def get_sprite(self, coords: tuple[int, int]) -> pygame.Surface:
+        x, y = coords
         return self.image.subsurface((x * 100, y * 100, 100, 100))
